dc7aed01d962e955c031088010d5fe60134e236a
Inori Sakura <inorindesu@gmail.com>
2013-05-01 00:40:39 +0800

    SVG output is almost done.
    Current problem is that my code did not consider negative coordinate.
    Thus, boundary detection is not correct.

--- main.c
+++ main.c
@@ -14,31 +14,98 @@
 #include <stdio.h>
 
 char defaultFont[] = "/usr/share/fonts/google-droid/DroidSans.ttf";
-unsigned int defaultChar = 'A';
+unsigned int defaultChar = 'm';
+
+int test_move_to(const FT_Vector* to, void* user)
+{
+  FT_Vector* vec = (FT_Vector*)user;
+  fprintf(stderr, "MOVE_TO: (%d, %d)\n", to->x, to->y);
+  if (to->x > vec->x)
+    vec->x = to->x;
+  if (to->y > vec->y)
+    vec->y = to->y;
+  fprintf(stderr, "%d %d\n", vec->x, vec->y);
+  return 0;
+}
+
+int test_line_to(const FT_Vector* to, void* user)
+{
+  FT_Vector* vec = (FT_Vector*)user;
+  if (to->x > vec->x)
+    vec->x = to->x;
+  if (to->y > vec->y)
+    vec->y = to->y;
+  fprintf(stderr, "%d %d\n", vec->x, vec->y);
+  return 0;
+}
+
+int test_conic_to(const FT_Vector* ctrl, const FT_Vector* to, void* user)
+{
+  FT_Vector* vec = (FT_Vector*)user;
+  if (ctrl->x > vec->x)
+    vec->x = ctrl->x;
+  if (ctrl->y > vec->y)
+    vec->y = ctrl->y;
+  if (to->x > vec->x)
+    vec->x = to->x;
+  if (to->y > vec->y)
+    vec->y = to->y;
+  fprintf(stderr, "%d %d\n", vec->x, vec->y);
+  return 0;
+}
+
+int test_cubic_to(const FT_Vector* ctrl1, const FT_Vector* ctrl2, const FT_Vector* to,
+             void* user)
+{
+  FT_Vector* vec = (FT_Vector*)user;
+  if (ctrl1->x > vec->x)
+    vec->x = ctrl1->x;
+  if (ctrl1->y > vec->y)
+    vec->y = ctrl1->y;
+  if (ctrl2->x > vec->x)
+    vec->x = ctrl2->x;
+  if (ctrl2->y > vec->y)
+    vec->y = ctrl2->y;
+  if (to->x > vec->x)
+    vec->x = to->x;
+  if (to->y > vec->y)
+    vec->y = to->y;
+  fprintf(stderr, "%d %d\n", vec->x, vec->y);
+  return 0;
+}
 
 int move_to(const FT_Vector* to, void* user)
 {
-  fprintf(stderr, "MOVE_TO: (%d, %d)\n", to->x, to->y);
+  FT_Vector* vec = (FT_Vector*)user;
+  fprintf(stderr, "MOVE_TO: (%d, %d)\n", to->x, vec->y - to->y);
+  printf(" M %d %d ", to->x, vec->y - to->y);
   return 0;
 }
 
 int line_to(const FT_Vector* to, void* user)
 {
-  fprintf(stderr, "LINE_TO: (%d, %d)\n", to->x, to->y);
+  FT_Vector* vec = (FT_Vector*)user;
+  fprintf(stderr, "LINE_TO: (%d, %d)\n", to->x, vec->y - to->y);
+  printf(" L %d %d ", to->x, vec->y - to->y);
   return 0;
 }
 
 int conic_to(const FT_Vector* ctrl, const FT_Vector* to, void* user)
 {
-  fprintf(stderr, "CONIC_TO: *(%d, %d) (%d, %d)\n", ctrl->x, ctrl->y, to->x, to->y);
+  FT_Vector* vec = (FT_Vector*)user;
+  fprintf(stderr, "CONIC_TO: *(%d, %d) (%d, %d)\n", ctrl->x, vec->y - ctrl->y, to->x, vec->y - to->y);
+  printf(" Q %d %d %d %d ", ctrl->x, vec->y - ctrl->y, to->x, vec->y - to->y);
   return 0;
 }
 
 int cubic_to(const FT_Vector* ctrl1, const FT_Vector* ctrl2, const FT_Vector* to,
              void* user)
 {
-  fprintf(stderr, "CUBIC_TO: *(%d, %d)*(%d, %d) (%d, %d)\n", ctrl1->x, ctrl1->y
-         , ctrl2->x, ctrl2->y, to->x, to->y);
+  FT_Vector* vec = (FT_Vector*)user;
+  fprintf(stderr, "CUBIC_TO: *(%d, %d)*(%d, %d) (%d, %d)\n", ctrl1->x, vec->y - ctrl1->y
+         , ctrl2->x, vec->y - ctrl2->y, to->x, vec->y - to->y);
+  printf(" C %d %d %d %d ", ctrl1->x, vec->y - ctrl1->y, ctrl2->x, vec->y - ctrl2->y, to->x
+         , vec->y - to->y);
   return 0;
 }
 
@@ -153,18 +220,40 @@ int main(int argc, char** argv)
       fprintf(stderr, "ERROR: I process only outline fonts.");
       exit(1);
     }
+
+  FT_Vector vec;
+  vec.x = 0;
+  vec.y = 0;
+
+  FT_Outline_Funcs testFuncs;
+  testFuncs.shift = 0;
+  testFuncs.delta = 0;
+  testFuncs.move_to = test_move_to;
+  testFuncs.line_to = test_line_to;
+  testFuncs.conic_to = test_conic_to;
+  testFuncs.cubic_to = test_cubic_to;
   
-  FT_Outline_Funcs funcs;
-  funcs.shift = 0;
-  funcs.delta = 0;
-  funcs.move_to = move_to;
-  funcs.line_to = line_to;
-  funcs.conic_to = conic_to;
-  funcs.cubic_to = cubic_to;
-  
+  FT_Outline_Funcs drawFuncs;
+  drawFuncs.shift = 0;
+  drawFuncs.delta = 0;
+  drawFuncs.move_to = move_to;
+  drawFuncs.line_to = line_to;
+  drawFuncs.conic_to = conic_to;
+  drawFuncs.cubic_to = cubic_to;
+
+  fprintf(stderr, "Testing height..\n");
+  FT_Outline_Decompose(&face->glyph->outline, &testFuncs, &vec);
+  fprintf(stderr, "Highest x is: %d\nHighest y is: %d\n", vec.x, vec.y);
+
   fprintf(stderr, "Generating data..\n");
   /* coordinates are in 26.6 format. */
-  FT_Outline_Decompose(&face->glyph->outline, &funcs, NULL);
+  printf("<?xml version=\"1.0\" standalone=\"no\"?>\n");
+  printf("<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"%f\" height=\"%f\">\n"
+         , vec.x / 64.0, vec.y / 64.0);
+  printf("<path transform=\"scale(%f)\" d =\"", 1 / 64.0);
+  FT_Outline_Decompose(&face->glyph->outline, &drawFuncs, &vec);
+  printf("\" id=\"char_%.8x\">\n", charToPlot);
+  printf("</path>\n</svg>\n");
   
   FT_Done_Face(face);
   FT_Done_FreeType(lib);
